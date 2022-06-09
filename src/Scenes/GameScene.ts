23ff708d72e73c63a1f2f08d416d1d576c560182
bi3mer <colan@comcast.net>
2022-06-09 10:33:38 -0400

    portal system

--- src/Scenes/GameScene.ts
+++ src/Scenes/GameScene.ts
@@ -5,30 +5,45 @@ import { Render } from "../Components/Render";
 import { Player } from "../Components/Player";
 import { LEVELS } from "../levels";
 import { RenderSystem } from "../Systems/RenderSystem";
+import { Portal } from "../Components/Portal";
+import { PortalSystem } from "../Systems/PortalSystem";
 
 export class GameScene extends ECSScene {
   public scenePlayerWonIndex: number = 0;
   public scenePlayerLostIndex: number = 0;
   private size: number = 20;
   private playerID: number = 0;
-  
 
   constructor() {
     super();
+    this.blackBoard.set('game over', 0);
   }
 
   public onEnter(game: Game): void {
+    let switchCount = 0;
     const lvlKey = game.blackBoard.get('level') as string;
     const lvl = LEVELS[lvlKey as keyof typeof LEVELS];
     for (let y = 0; y < lvl.length; ++y) {
       for (let x = 0; x < lvl[y].length; ++x) {
         const char = lvl[y][x];
         const id = this.addEntity();
+        
         this.addComponent(id, new Render(char));
         this.addComponent(id, new Position(x, y));
+        
+        if (char == 'O') {
+          this.addComponent(id, new Portal());
+        } else if (char == '@') {
+          this.addComponent(id, new Player());
+        } else if (char == '*') {
+          switchCount += 1;
+        }
       }
     }
 
+    this.blackBoard.set('switch count', switchCount);
+
+    this.addSystem(90, new PortalSystem());
     this.addSystem(100, new RenderSystem());
   }
   
@@ -37,10 +52,13 @@ export class GameScene extends ECSScene {
   }
 
   public customUpdate(game: Game): number {
-    // const playerPos = this.getComponents(this.playerID).get(Position);
-    // if (playerPos.x == this.size - 1 && playerPos.y == this.size - 1) {
-    //   return -1;
-    // }
+    const gameOver = this.blackBoard.get('game over')
+    if (gameOver == -1) {
+      return this.scenePlayerLostIndex;
+    } else if (gameOver == 1) {
+      return this.scenePlayerWonIndex;
+    }
+
     return -1;
   }
 }
---
+++ src/Systems/PortalSystem.ts
@@ -0,0 +1,18 @@
+import { Portal } from "../Components/Portal";
+import { Render } from "../Components/Render";
+import { Entity } from "../Engine/Entity";
+import { Game } from "../Engine/Game";
+import { System } from "../Engine/System";
+
+export class PortalSystem extends System {
+  componentsRequired = new Set<Function>([Portal, Render]);
+
+  update(game: Game, entities: Set<Entity>): void {
+    const [id] = entities; // there can only be one portal
+    if(this.ecs.blackBoard.get('switch count') == 0) {
+      this.ecs.getComponents(id).get(Render).character = 'O';
+    } else {
+      this.ecs.getComponents(id).get(Render).character = 'o';
+    }
+  }
+}
